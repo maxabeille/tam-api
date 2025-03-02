8c36d7ee776b5b8d7236d4dad125efa62f50714c
alexis-mateo <alexis.mateo@etu.umontpellier.fr>
2025-03-03 00:22:55 +0100

    fix var name

--- src/responseActions/nextTramsToDirection.js
+++ src/responseActions/nextTramsToDirection.js
@@ -1,14 +1,14 @@
 export const nextTramsToDirection = async (req, res) => {
-  const destination = req?.body?.fulfillmentInfo?.parameters?.destination[0]?.original;
-  if (!destination) {
+  const direction = req?.body?.fulfillmentInfo?.parameters?.direction[0]?.original;
+  if (!direction) {
     return 'Je ne peux pas récupérer la destination.';
   }
-  if (!['mosson', 'odysseum'].includes(destination)) {
+  if (!['mosson', 'odysseum'].includes(direction)) {
     return 'Je ne peux pas récupérer les horaires pour cette destination.';
   }
 
-  const response = await fetch(`https://tam.alexis-mateo.fr/stop/next/${encodeURIComponent('pl. de l\'europe')}?line=1&destination=${destination}`);
+  const response = await fetch(`https://tam.alexis-mateo.fr/stop/next/${encodeURIComponent('pl. de l\'europe')}?line=1&destination=${direction}`);
   const data = await response.json();
   const nextTram = data[0].duration.min;
-  return `Le prochain tram vers ${destination} arrive dans ${nextTram} minutes.`
+  return `Le prochain tram vers ${direction} arrive dans ${nextTram} minutes.`
 } 
