9e15c87d95794db28bec857fbf12181565c66682
alexis-mateo <alexis.mateo@etu.umontpellier.fr>
2025-03-02 23:44:35 +0100

    remove res send

--- src/responseActions/nextTram.js
+++ src/responseActions/nextTram.js
@@ -2,5 +2,5 @@ export const nextTram = async (req, res) => {
   const response = await fetch(`https://tam.alexis-mateo.fr/stop/next/${encodeURIComponent('pl. de l\'europe')}?line=1&destination=mosson`);
   const data = await response.json();
   const nextTram = data[0].duration.min;
-  return res.json({ fulfillmentText: `Le prochain tram vers Mosson arrive dans ${nextTram} minutes.` });
+  return `Le prochain tram vers Mosson arrive dans ${nextTram} minutes.`
 } 
--- src/responseActions/nextTrams.js
+++ src/responseActions/nextTrams.js
@@ -1,6 +1,5 @@
 export const nextTrams = async (req, res) => {
   const response = await fetch(`https://tam.alexis-mateo.fr/stop/next/${encodeURIComponent('pl. de l\'europe')}?line=1&destination=mosson`);
   const data = await response.json();
-  const result = `Les prochains trams vers Mosson arrive dans ${data.map(data => data.duration.min).join(', ')} minutes.`;
-  return res.json({ fulfillmentText: result });
+  return `Les prochains trams vers Mosson arrive dans ${data.map(data => data.duration.min).join(', ')} minutes.`;
 } 
