81a550c729c053bbe96819bfc22d280bd8682451
alexis-mateo <alexis.mateo@etu.umontpellier.fr>
2025-03-02 22:25:32 +0100

    add minutes

--- src/router/webhookRouter.js
+++ src/router/webhookRouter.js
@@ -11,9 +11,8 @@ webhookRouter.post('/', async (req, res) => {
     try {
         const response = await fetch(`https://tam.alexis-mateo.fr/stop/next/${encodeURIComponent('pl. de l\'europe')}?line=1&destination=mosson`);
         const data = await response.json();
-        const nextTram = data[0].duration;
-        res.json({ fulfillmentText: `Le prochain tram arrive dans toto minutes.` });
-        // res.json({ fulfillmentText: `Le prochain tram arrive dans ${nextTram} minutes.` });
+        const nextTram = data[0].duration.min;
+        res.json({ fulfillmentText: `Le prochain tram arrive dans ${nextTram} minutes.` });
     } catch (error) {
         console.error(error);
         res.json({ fulfillmentText: "Je ne peux pas récupérer les horaires pour le moment." });
