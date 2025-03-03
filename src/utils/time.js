51d7805e6579681d64a78c9b2c6f8ec5728fda3e
alexis-mateo <alexis.mateo@etu.umontpellier.fr>
2025-03-03 12:44:34 +0100

    fix strange min in duration

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,18 +1,25 @@
 export const parseTime = (time) => {
-    const now = new Date();
+  const now = new Date();
 
-    const [hours, minutes, seconds] = time.split(':').map(Number);
-    const stopTime = new Date(now);
-    stopTime.setHours(hours, minutes, seconds, 0);
+  const [hours, minutes, seconds] = time.split(':').map(Number);
+  const stopTime = new Date(now);
+  stopTime.setHours(hours, minutes, seconds, 0);
 
-    if (stopTime < now) {
+  let deltaMs = stopTime - now;
+  
+  // Si l'heure est passée mais dans une plage de 30 minutes, on considère que le tram est en retard
+  if (deltaMs < 0 && Math.abs(deltaMs) < 30 * 60 * 1000) {
+      return { min: 0, sec: 0 };
+  }
+
+  // Si l'heure est passée et hors plage, c'est l'horaire du lendemain
+  if (deltaMs < 0) {
       stopTime.setDate(stopTime.getDate() + 1);
-    }
-    const deltaMs = stopTime - now;
-    const deltaMinutes = Math.floor(deltaMs / (1000 * 60));
-    const deltaSeconds = Math.floor((deltaMs % (1000 * 60)) / 1000);
-    return {
-      min: deltaMinutes,
-      sec: deltaSeconds
-    }
-}
+      deltaMs = stopTime - now;
+  }
+
+  return {
+    min: Math.floor(deltaMs / (1000 * 60)),
+    sec: Math.floor((deltaMs % (1000 * 60)) / 1000)
+  };
+};
