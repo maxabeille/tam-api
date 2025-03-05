04ddfe612e4b9a05dc06c88b16ddd77b48b3cab2
alexis-mateo <alexis.mateo@etu.umontpellier.fr>
2025-03-05 13:54:30 +0100

    fix local date format

--- src/router/stopRouter.js
+++ src/router/stopRouter.js
@@ -54,14 +54,15 @@ stopRouter.get('/schedule/:station/:line/:direction', async (req, res) => {
     return res.status(404).json({error: 'Stop not found'})
   }
 
-  fetch(`${baseURL}&lign_id=${lineId}&sens=${req.params.direction}&laDate=${encodeURIComponent(new Date().toLocaleDateString())}&pa_id=${stopId}`)
+  const url = `${baseURL}&lign_id=${lineId}&sens=${req.params.direction}&laDate=${encodeURIComponent(new Date().toLocaleDateString('fr-FR'))}&pa_id=${stopId}`
+  fetch(url)
     .then(response => {
       return response.text()
     }).then(text => {
       const root = htmlParse(text)
       const table = root.getElementById('stophour')
       if(!table) {
-        return res.status(404).json({error: 'Schedule not found at this url : ' + `${baseURL}&lign_id=${lineId}&sens=${req.params.direction}&laDate=${encodeURIComponent(new Date().toLocaleDateString())}&pa_id=${stopId}`})
+        return res.status(404).json({error: `Schedule not found at this url : ${url}`})
       }
 
       const schedule = {};
